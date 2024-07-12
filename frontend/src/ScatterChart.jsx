84a4fe266ee4018bcf4810e82ac258876f1c6f45
Monica Anthony <monicaanthony@Monicas-MacBook-Air.local>
2024-07-12 02:23:16 -0400

    feat:slider

--- frontend/src/ScatterChart.jsx
+++ frontend/src/ScatterChart.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import {
   ScatterChart,
   Scatter,
@@ -8,13 +8,23 @@ import {
   CartesianGrid,
   ResponsiveContainer,
 } from 'recharts';
+import Slider from '@mui/material/Slider';
 
 const SimpleScatterChart = ({data}) => {
   const [selectedMetric, setSelectedMetric] = useState('sepal.width');
+  const [yBounds, setYBounds] = useState([0, 10]);
+  const maxYValue = Math.max(...data.map(item => item[selectedMetric] || 0));
 
   const handleCheckboxChange = (metric) => {
     setSelectedMetric(metric);
   };
+
+
+  useEffect(() => {
+    const maxYValue = Math.max(...data.map(item => item[selectedMetric] || 0));
+    setYBounds([0, maxYValue]);
+  }, [selectedMetric, data]);
+
 
   return (
     <>
@@ -50,11 +60,22 @@ const SimpleScatterChart = ({data}) => {
           Petal Width
         </label>
       </div>
+      <div style={{ margin: '20px 0' }}>
+        <span>Y Axis Bounds: {yBounds[0]} - {yBounds[1]}</span>
+        <Slider
+          value={yBounds}
+          onChange={(e, newValue) => setYBounds(newValue)}
+          min={0}
+          max={maxYValue}
+          step={0.01}
+          valueLabelDisplay="auto"
+        />
+      </div>
     <ResponsiveContainer width="100%" height={400}>
       <ScatterChart>
         <CartesianGrid />
         <XAxis type="number" dataKey="sepal.length" name="X Axis" />
-        <YAxis type="number" dataKey={selectedMetric} name="Y Axis" />
+        <YAxis type="number" dataKey={selectedMetric} domain={yBounds} name="Y Axis" />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name="Sample Data" data={data} fill="#8884d8" />
       </ScatterChart>
